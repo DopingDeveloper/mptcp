51ece9cc07aa7afab360ba794228ece906be7751
YS Cho <dkoepower@gmail.com>
2015-11-17 10:57:12 +0900

    add iperf server router

--- arch/sim/test/buildtop/source/ns-3-dce/example/dce-iperf-mptcp.cc
+++ arch/sim/test/buildtop/source/ns-3-dce/example/dce-iperf-mptcp.cc
@@ -23,13 +23,12 @@ int main (int argc, char *argv[])
   CommandLine cmd;
   cmd.AddValue ("nRtrs", "Number of routers. Default 2", nRtrs);
   cmd.Parse (argc, argv);
-//  nRtrs *= 2;
-  cout << nRtrs << endl;
-  NodeContainer nodes, routers, interfers;
+
+  NodeContainer nodes, routers, interfers, server_router;
   nodes.Create (2);
   routers.Create (nRtrs);
   interfers.Create (nRtrs);
-
+  server_router.Create (1);
 
   DceManagerHelper dceManager;
   dceManager.SetTaskManagerAttribute ("FiberManagerType",
@@ -41,20 +40,24 @@ int main (int argc, char *argv[])
   stack.Install (nodes);
   stack.Install (routers);
   stack.Install (interfers);
+  stack.Install (server_router);
 
   dceManager.Install (nodes);
   dceManager.Install (routers);
   dceManager.Install (interfers);
+  dceManager.Install (server_router);
 
   PointToPointHelper pointToPoint;
-  NetDeviceContainer devices1, devices2, devices3, devices4, devices5;
-  Ipv4AddressHelper address1, address2, address3, address4, address5;
+  NetDeviceContainer devices1, devices2, devices3, devices4, devices5, devices6;
+  Ipv4AddressHelper address1, address2, address3, address4, address5, address6;
   std::ostringstream cmd_oss;
+
   address1.SetBase ("10.1.0.0", "255.255.255.0");
   address2.SetBase ("10.2.0.0", "255.255.255.0");
   address3.SetBase ("10.3.0.0", "255.255.255.0");
   address4.SetBase ("10.4.0.0", "255.255.255.0");
   address5.SetBase ("10.5.0.0", "255.255.255.0");
+  address6.SetBase ("10.6.0.0", "255.255.255.0");
 
   for (uint32_t i = 0; i < nRtrs; i+=2)
     {
@@ -69,44 +72,41 @@ int main (int argc, char *argv[])
       cmd_oss.str ("");
       cmd_oss << "rule add from " << if1.GetAddress (0, 0) << " table " << (i/2+1);
       LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Node 0 : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.1." << i/2 << ".0/24 dev sim" << i/2 << " scope link table " << (i/2+1);
       LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), cmd_oss.str ().c_str ());
-//     cout << "Node 0 : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add default via " << if1.GetAddress (1, 0) << " dev sim" << i/2 << " table " << (i/2+1);
       LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Node 0 : " << cmd_oss.str().c_str() << endl;
+
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.1." << i/2 << ".0/24 via " << if1.GetAddress (1, 0) << " dev sim0";
       LinuxStackHelper::RunIp (routers.Get (i), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i << " : " << cmd_oss.str().c_str() << endl;
+
 
       // Right link
       pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
       pointToPoint.SetChannelAttribute ("Delay", StringValue ("1ns"));
-      devices2 = pointToPoint.Install (nodes.Get (1), routers.Get (i+1));
+      devices2 = pointToPoint.Install (server_router.Get(0), routers.Get (i+1));
       // Assign ip addresses
       Ipv4InterfaceContainer if2 = address2.Assign (devices2);
       address2.NewNetwork ();
       // setup ip routes
+
       cmd_oss.str ("");
-      cmd_oss << "rule add from " << if2.GetAddress (0, 0) << " table " << (i/2+1);
-      LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Node 1 : " << cmd_oss.str().c_str() << endl;
+      cmd_oss << "route add 10.1." <<i/2 << ".0/24 via " << if2.GetAddress (1, 0) << " dev sim" << i/2;
+      LinuxStackHelper::RunIp (server_router.Get(0), Seconds (0.2), cmd_oss.str ().c_str ());
+
       cmd_oss.str ("");
-      cmd_oss << "route add 10.2." << i/2 << ".0/24 dev sim" << i/2 << " scope link table " << (i/2+1);
-      LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Node 1 : " << cmd_oss.str().c_str() << endl;
-      cmd_oss.str ("");
-      cmd_oss << "route add default via " << if2.GetAddress (1, 0) << " dev sim" << i/2 << " table " << (i/2+1);
-      LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Node 1 : " << cmd_oss.str().c_str() << endl;
+      cmd_oss << "route add 10.6.0.0/16 via " << if2.GetAddress (1, 0) << " dev sim0";
+      LinuxStackHelper::RunIp (routers.Get (i+1), Seconds (0.2), cmd_oss.str ().c_str ());
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.2." <<i/2 << ".0/24 via " << if2.GetAddress (1, 0) << " dev sim0";
       LinuxStackHelper::RunIp (routers.Get (i+1), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i+1 << " : " << cmd_oss.str().c_str() << endl;
 
       // Center link
       pointToPoint.SetDeviceAttribute ("DataRate", StringValue("1Gbps"));
@@ -117,13 +117,16 @@ int main (int argc, char *argv[])
       address3.NewNetwork ();
       // setup ip routes
       cmd_oss.str("");
+      cmd_oss << "route add 10.6.0.0/16 via " << if3.GetAddress(1, 0) << " dev sim1";
+      LinuxStackHelper::RunIp (routers.Get(i), Seconds (0.3), cmd_oss.str ().c_str ());
+
+      cmd_oss.str("");
       cmd_oss << "route add 10.2." << i/2 << ".0/24 via " << if3.GetAddress(1, 0) << " dev sim1";
       LinuxStackHelper::RunIp (routers.Get(i), Seconds (0.3), cmd_oss.str ().c_str ());
-//      cout << "Router " << i << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str("");
       cmd_oss << "route add 10.1." << i/2 << ".0/24 via " << if3.GetAddress(0, 0) << " dev sim1";
       LinuxStackHelper::RunIp (routers.Get(i+1), Seconds (0.3), cmd_oss.str ().c_str ());
-//      cout << "Router " << i+1 << " : " << cmd_oss.str().c_str() << endl;
 
       //Interfering Sender
       pointToPoint.SetDeviceAttribute ("DataRate", StringValue("100Mbps"));
@@ -136,23 +139,22 @@ int main (int argc, char *argv[])
       cmd_oss.str ("");
       cmd_oss << "rule add from " << if4.GetAddress (0, 0);
       LinuxStackHelper::RunIp (interfers.Get (i), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.4." << i/2 << ".0/24 dev sim0 scope link";
       LinuxStackHelper::RunIp (interfers.Get (i), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.5."<< i/2 <<".0/24 via " << if4.GetAddress (1, 0) << " dev sim0";
       LinuxStackHelper::RunIp (interfers.Get (i), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.4." <<i/2 << ".0/24 via " << if4.GetAddress (1, 0) << " dev sim2";
       LinuxStackHelper::RunIp (routers.Get (i), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.5." <<i/2 << ".0/24 via " << if3.GetAddress (1, 0) << " dev sim1";
       LinuxStackHelper::RunIp (routers.Get (i), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i << " : " << cmd_oss.str().c_str() << endl;
 
 
       //Interfering Receiver
@@ -166,23 +168,22 @@ int main (int argc, char *argv[])
       cmd_oss.str ("");
       cmd_oss << "rule add from " << if5.GetAddress (0, 0);
       LinuxStackHelper::RunIp (interfers.Get (i+1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i+1 << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.5." << i/2 << ".0/24 dev sim0 scope link";
       LinuxStackHelper::RunIp (interfers.Get (i+1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i+1 << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.4."<< i/2 <<".0/24 via " << if5.GetAddress (1, 0) << " dev sim0";
       LinuxStackHelper::RunIp (interfers.Get (i+1), Seconds (0.1), cmd_oss.str ().c_str ());
-//      cout << "Interfers " << i+1 << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.5." <<i/2 << ".0/24 via " << if5.GetAddress (1, 0) << " dev sim2";
       LinuxStackHelper::RunIp (routers.Get (i+1), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i+1 << " : " << cmd_oss.str().c_str() << endl;
+
       cmd_oss.str ("");
       cmd_oss << "route add 10.4." <<i/2 << ".0/24 via " << if3.GetAddress (0, 0) << " dev sim1";
       LinuxStackHelper::RunIp (routers.Get (i+1), Seconds (0.2), cmd_oss.str ().c_str ());
-//      cout << "Router " << i+1 << " : " << cmd_oss.str().c_str() << endl;
 
       setPos (routers.Get (i),   25, i * 20, 0);
       setPos (routers.Get (i+1), 75, i * 20, 0);
@@ -190,9 +191,34 @@ int main (int argc, char *argv[])
       setPos (interfers.Get (i+1), 65, (i+0.5) * 20, 0);
     }
 
+  // Server link
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("10ms"));
+  devices6 = pointToPoint.Install (nodes.Get(1), server_router.Get(0));
+  // Assign ip addresses
+  Ipv4InterfaceContainer if6 = address6.Assign (devices6);
+  address6.NewNetwork ();
+
+  // setup ip routes
+  cmd_oss.str ("");
+  cmd_oss << "rule add from " << if6.GetAddress (0, 0);
+  LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
+
+  cmd_oss.str ("");
+  cmd_oss << "route add 10.6.0.0/16 dev sim scope link";
+  LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
+
+  cmd_oss.str ("");
+  cmd_oss << "route add default via " << if6.GetAddress (1, 0) << " dev sim0";
+  LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
+
+  cmd_oss.str ("");
+  cmd_oss << "route add 10.6.0.0/16 via " << if6.GetAddress (1, 0) << " dev sim" << (nRtrs/2) << " table " << (nRtrs/2 + 1);
+  LinuxStackHelper::RunIp (server_router.Get(0), Seconds (0.2), cmd_oss.str ().c_str ());
+
   // default route
   LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), "route add default via 10.1.0.2 dev sim0");
-  LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), "route add default via 10.2.0.2 dev sim0");
+  LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), "route add default via 10.6.0.2 dev sim0");
   LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), "rule show");
 
   // Schedule Up/Down (XXX: didn't work...)
@@ -215,7 +241,7 @@ int main (int argc, char *argv[])
   dce.ResetArguments ();
   dce.ResetEnvironment ();
   dce.AddArgument ("-c");
-  dce.AddArgument ("10.2.0.1");
+  dce.AddArgument ("10.6.0.1");
   dce.AddArgument ("-i");
   dce.AddArgument ("1");
   dce.AddArgument ("--time");
@@ -257,7 +283,8 @@ int main (int argc, char *argv[])
     }
 
   setPos (nodes.Get (0), 0, 20 * (nRtrs - 1) / 2, 0);
-  setPos (nodes.Get (1), 100, 20 * (nRtrs - 1) / 2, 0);
+  setPos (nodes.Get (1), 120, 20 * (nRtrs - 1) / 2, 0);
+  setPos (server_router.Get(0), 100, 20 * (nRtrs - 1) / 2, 0);
 
   Simulator::Stop (Seconds (50.0));
   Simulator::Run ();
